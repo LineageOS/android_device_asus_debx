53be2ab00f66ce6d66863495cce94892f256bdd5
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-02 19:39:50 -0800

    debx: Set shipping API level via inherited makefile
    * PRODUCT_SHIPPING_API_LEVEL is set there.
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15

--- lineage_debx.mk
+++ lineage_debx.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit device configuration
 $(call inherit-product, device/asus/debx/device.mk)
 
+# Shipping API
+$(call inherit-product, vendor/lineage/build/target/product/product_launched_with_j_mr2.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := debx
 PRODUCT_NAME := lineage_debx
