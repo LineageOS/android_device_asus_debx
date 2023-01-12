4e33206e68b8e621bbf2ffc615fa1fd66d8d0cbe
SebaUbuntu <seba@sebaubuntu.dev>
2023-01-12 22:50:33 +0530

    debx: move to  SPDX license identifier
    - and add missing licenses
    
    Change-Id: I11368c92b74747abf773935ba3573931492a5cbd

--- device.mk
+++ device.mk
@@ -1,17 +1,8 @@
 #
 # Copyright 2013 The Android Open-Source Project
+# Copyright (C) 2023 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # rild
@@ -37,7 +28,7 @@ $(call inherit-product, device/asus/flox/device-common.mk)
 # inherit from the non-open-source side, if present
 $(call inherit-product-if-exists, vendor/asus/debx/debx-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \ 
+DEVICE_PACKAGE_OVERLAYS += \
     device/asus/debx/overlay \
     device/asus/flox/overlay-lineage
 
