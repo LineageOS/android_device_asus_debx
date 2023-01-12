e3122f23166b74a70da9abc5fa7a8940ce551c08
hpnightowl <iamhp2k@gmail.com>
2023-01-12 23:07:43 +0530

    debx: restructure mk files with labels
    Change-Id: I5a3559f0d151f6ac00da0b1d034d53f6d57157e5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,20 +4,27 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Inherit from flox
+include device/asus/flox/BoardConfigCommon.mk
 
+# Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := deb
 TARGET_BOOTLOADER_NAME := deb
 TARGET_BOARD_INFO_FILE := device/asus/debx/board-info.txt
 
-TARGET_RELEASETOOLS_EXTENSIONS := device/asus/debx
-
-TARGET_SYSTEM_PROP += \
+# Properties
+TARGET_SYSTEM_PROP := \
     device/asus/flox/system.prop \
     device/asus/debx/system.prop
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/asus/debx
+
+# Recovery
 TARGET_RECOVERY_FSTAB = device/asus/debx/rootdir/etc/fstab.debx
 
+# Sepolicy
 BOARD_SEPOLICY_DIRS += device/asus/debx/sepolicy
 
--include vendor/asus/debx/BoardConfigVendor.mk
-include device/asus/flox/BoardConfigCommon.mk
+# Inherit the proprietary files
+include vendor/asus/debx/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -5,33 +5,36 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# rild
-PRODUCT_PACKAGES := \
-    rild \
-    CarrierConfig \
-    BasicSmsReceiver
+# Inherit from flox
+$(call inherit-product, device/asus/flox/device-common.mk)
 
-# Features
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
+# Inherit the proprietary files
+$(call inherit-product, vendor/asus/debx/debx-vendor.mk)
 
 # Camera
 PRODUCT_PACKAGES += \
     camera.debx
 
-PRODUCT_PACKAGES += \
-    hostapd_default.conf
-
-# the actual meat of the device-specific product definition
-$(call inherit-product, device/asus/flox/device-common.mk)
-
-# inherit from the non-open-source side, if present
-$(call inherit-product-if-exists, vendor/asus/debx/debx-vendor.mk)
-
+# Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     device/asus/debx/overlay \
     device/asus/flox/overlay-lineage
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
+
+# Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.debx:$(TARGET_COPY_OUT_RAMDISK)/fstab.flox \
     $(LOCAL_PATH)/rootdir/etc/fstab.debx:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.flox
+
+# Rild
+PRODUCT_PACKAGES += \
+    rild \
+    CarrierConfig \
+    BasicSmsReceiver
+
+# Wifi
+PRODUCT_PACKAGES += \
+    hostapd_default.conf
