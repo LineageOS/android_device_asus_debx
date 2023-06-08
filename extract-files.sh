3cf97bce2eb4761b77313ca9d6cd79daff96643a
Nolen Johnson <johnsonnolen@gmail.com>
2023-06-08 01:24:08 -0400

    debx: Call flox extract-files after we're done
    Change-Id: I128ca8e005faa99154c51e4b5984d5630d0778d0

--- extract-files.sh
+++ extract-files.sh
@@ -59,3 +59,5 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
+
+"${MY_DIR}/../flox/extract-files.sh" "${SRC}"
